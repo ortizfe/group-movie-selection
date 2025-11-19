454849e73e64f51713e7062ecef8bf1535c3ca08
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-11-19 00:04:56 -0500

    making landing page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GlobalStyles, StyledEngineProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <App />
+    </StyledEngineProvider>
+  </StrictMode>
+);
